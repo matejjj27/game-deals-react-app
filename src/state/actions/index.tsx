adc7b6fb26e2605f070bbdb34dacca595698a6e5
Matej Kostov <matej4o9@gmail.com>
2023-01-18 14:29:38 +0200

    Add store to favorites using Redux #26

--- src/state/actions/index.tsx
+++ src/state/actions/index.tsx
@@ -19,25 +19,63 @@ export const logIn = () => {
     return {
         type: "LOG_IN"
     }
-}
+};
 
 interface FetchStoresSuccessAction {
     type: 'FETCH_USERS_SUCCESS';
     payload: IStore[];
-}
+};
   
 interface FetchStoresErrorAction {
     type: 'FETCH_USERS_ERROR';
     payload: string;
-}
+};
 
-type FetchUsersAction = FetchStoresSuccessAction | FetchStoresErrorAction
+type FetchStoresAction = FetchStoresSuccessAction | FetchStoresErrorAction;
   
-export const fetchStores = async (dispatch: any): Promise<FetchUsersAction> => {
+export const fetchStores = async (dispatch: any): Promise<FetchStoresAction> => {
     try {
         const response = await axios.get('https://www.cheapshark.com/api/1.0/stores');
         return dispatch({ type: 'FETCH_STORES_SUCCESS', payload: response.data.slice(0, 12) });
     } catch (error) {
-        return dispatch({ type: 'FETCH_STORES_FAIL', payload: error });
+        return dispatch({ type: 'FETCH_STORES_ERROR', payload: error });
     }
-}
+};
+
+interface SelectStoresSuccessAction {
+    type: 'FETCH_USERS_SUCCESS';
+    payload: IStore[];
+};
+  
+interface SelectStoresErrorAction {
+    type: 'FETCH_USERS_ERROR';
+    payload: string;
+};
+
+type SelectStoresAction = SelectStoresSuccessAction | SelectStoresErrorAction;
+
+export const selectStore = (dispatch: any, store: IStore, stores: Array<IStore>): Promise<SelectStoresAction> => {
+    try {
+        return dispatch({ type: 'SELECT_STORE_SUCCESS', payload: addToFavorites(stores, store, "add") });
+    } catch (error) {
+        return dispatch({ type: 'SELECT_STORE_ERROR', payload: error });
+    }
+};
+
+const addToFavorites = (stores: Array<IStore>, store: IStore, addOrDel: string) => {
+    if(addOrDel === "add") {
+        let isFavorite = false;
+
+        for (let i=0; i<stores.length; i++) {
+            if(stores[i].storeID === store.storeID)
+                isFavorite = true;
+        }
+        if(isFavorite === false)
+            return stores.concat(store);
+        else 
+            return stores;
+    }
+    else if(addOrDel === "del"){
+        return stores.filter(item => item.storeID !== store.storeID);
+    }
+};
--- src/state/reducers/fetchStores.tsx
+++ src/state/reducers/fetchStores.tsx
@@ -1,21 +1,26 @@
 import { Reducer } from "@reduxjs/toolkit";
 
 interface State {
-    stores: IStore[];
+    stores: Array<IStore>,
+    selectedStores: Array<IStore>,
+    favoriteQuantity: number,
 }
   
 export interface IStore {
+    key: number,
     storeID: number;
     isActive: boolean,
-    storeName: string;
+    storeName: string,
+    isFavorite?: boolean,
     images: {
         banner: string,
         logo: string,
         icon: string,
     },
+    handleClick?: () => void,
 };
 
-const defaultState: State = {stores: []};
+const defaultState: State = {stores: [], selectedStores: [], favoriteQuantity: 0};
 
 const storesReducer: Reducer<State> = (state = defaultState, action) => {
     switch(action.type) {
@@ -24,10 +29,21 @@ const storesReducer: Reducer<State> = (state = defaultState, action) => {
                 ...state,
                 stores: action.payload,
             }
-        case "FETCH_STORES_FAIL":
+        case "FETCH_STORES_ERROR":
             return {
                 ...state,
                 stores: action.payload,
+            }
+        case "SELECT_STORE_SUCCESS":
+            return {
+                ...state,
+                selectedStores: action.payload,
+                favoriteQuantity: state.selectedStores.length + 1,
+            }
+        case "SELECT_STORE_ERROR":
+            return {
+                ...state,
+                selectedStores: action.payload,
             }
         default:
             return state;
