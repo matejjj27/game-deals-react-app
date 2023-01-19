97ff5ef61ad5ddc93ad47155fa73b4c2e1ba725d
Matej Kostov <matej4o9@gmail.com>
2023-01-19 13:40:04 +0200

    show cart only when it has content, show username only when logged in

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,17 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '..';
 import { useUser } from '../hooks/use-user';
 
 const Home: React.FC = () => {
 
     const { user } = useUser();
+
+    const isLogged = useSelector<RootState, boolean>((state) => state.isLogged.value);
     
     return(
         <div className='games'>
             <br></br>
-            <h1>Welcome {user}!</h1>
+            <h1>Welcome {isLogged ? user : "Guest"}!</h1>
         </div>
     )
 }
